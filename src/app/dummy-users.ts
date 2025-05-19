729b3f4941b14778c506c671487efce52198e4f3
Šárka F. <sarka.flidrova@email.cz>
2025-05-19 16:38:17 +0200

    Add user component and display random user

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Pansy Parkinson',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Hermione Granger',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Draco Malfoy',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Oliver Wood',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Luna Lovegood',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Theodore Nott',
+    avatar: 'user-6.jpg',
+  },
+];
