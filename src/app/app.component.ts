d37698b9488d3185c5c1a4fc0518dd4d191d4f1f
Šárka F. <sarka.flidrova@email.cz>
2025-05-19 15:29:16 +0200

    Add header component and styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [HeaderComponent],
 })
-export class AppComponent {
-  title = 'todo-angular';
-}
+export class AppComponent {}
