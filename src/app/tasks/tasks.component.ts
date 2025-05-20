f97b74d07628221431a5d23b8f6723e30122cbcc
Šárka F. <sarka.flidrova@email.cz>
2025-05-20 09:46:53 +0200

    Add a new task to the user on ngSubmit

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DUMMY_TASKS } from '../dummy-tasks';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { NewTaskData } from './tasks.model';
 
 @Component({
   selector: 'app-tasks',
@@ -29,6 +30,17 @@ export class TasksComponent {
   }
 
   onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate,
+    });
     this.isAddingTask = false;
   }
 }
